d146682e0e5ea5f2bfd719166f264c64cbaf6cb6
Andrew <hsmel73@mail.ru>
2017-03-30 23:46:42 +0300

    Add a StaticPage controller

---
+++ app/controllers/static_page_controller.rb
@@ -0,0 +1,8 @@
+# The StaticPageController controller is responsible for dispaly static page like 'home' and 'help'
+class StaticPageController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+end
