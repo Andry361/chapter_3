527f2204c46fe866814d501fc077c7a013e3357e
Andrew <hsmel73@mail.ru>
2017-04-01 15:28:24 +0300

    Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -24,6 +24,7 @@ describe "Static Pages" do
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
+
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
@@ -33,6 +34,18 @@ describe "Static Pages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+  describe "Contact page" do
+    it 'should have the content "Contact"' do
+      visit "/static_pages/contact"
+      expect(page).to have_content("Contact")
+    end
+
+    it 'should have the title "Contact"' do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end
